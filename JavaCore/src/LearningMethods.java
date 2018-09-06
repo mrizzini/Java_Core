5292ebeafea2d3d7ea2389019116ecd76a04e7a3
Matthew Rizzini <matthewrizzini@gmail.com>
2018-09-06 14:36:01 -0400

    Learning methods

---
+++ JavaCore/src/LearningMethods.java
@@ -0,0 +1,30 @@
+
+public class LearningMethods {
+
+	public void show() {
+		int a;
+		int b;
+		String c;
+	}
+	
+//	Display method
+	public int display(int k, String a, char c, float f, boolean b) // declaring --> define
+	{
+		System.out.println("Displaying message inside Display Method");
+		
+		return 10;
+		
+	}
+	
+	public static void main(String[] args) {
+
+		LearningMethods learn = new LearningMethods();
+		learn.show();
+		
+		LearningMethods learn1 = new LearningMethods();
+		learn1.show();
+		
+		
+	}
+
+}
